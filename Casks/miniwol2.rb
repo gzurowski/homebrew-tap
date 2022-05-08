d4fb5f7e6d44df33d065952abd2799a8b97f3af4
Gregor Zurowski <gregor@zurowski.net>
2022-05-08 09:09:29 +0200

    Add initial version of miniWOL cask

---
+++ Casks/miniwol2.rb
@@ -0,0 +1,11 @@
+cask "miniwol2" do
+    version "2.1.3"
+    sha256 "3a42570e704616620a74969ce180151553751bbcd33e34cd02194ac4edf50b3a"
+
+    url "https://www.tweaking4all.com/downloads/network/miniWOL-v2.1.3-64bit.dmg"
+    name "miniWOL v2"
+    desc "A small menu bar tool for sending Wake on LAN (WOL) network packets to hosts."
+    homepage "https://www.tweaking4all.com/network-internet/miniwol2/"
+
+    app "miniWOL-v2.1.3-64bit.dmg"
+end
