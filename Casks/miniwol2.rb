7676971bd0b5de9b7c0f0afb6fe0133a5c58c7a0
Gregor Zurowski <gregor@zurowski.net>
2022-05-08 10:17:43 +0200

    Use placeholder for version

--- Casks/miniwol2.rb
+++ Casks/miniwol2.rb
@@ -2,7 +2,7 @@ cask "miniwol2" do
     version "2.1.3"
     sha256 "3a42570e704616620a74969ce180151553751bbcd33e34cd02194ac4edf50b3a"
 
-    url "https://www.tweaking4all.com/downloads/network/miniWOL-v2.1.3-64bit.dmg"
+    url "https://www.tweaking4all.com/downloads/network/miniWOL-v#{version}-64bit.dmg"
     name "miniWOL v2"
     desc "A small menu bar tool for sending Wake on LAN (WOL) network packets to hosts."
     homepage "https://www.tweaking4all.com/network-internet/miniwol2/"
